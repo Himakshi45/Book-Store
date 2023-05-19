9b5dd4eefd05939d06e61326e3d62df49e5144a6
ilix-studio <ilishjyoti17@gmail.com>
2023-05-20 04:53:19 +0530

    userRegisted api tested +ve

--- server/server.js
+++ server/server.js
@@ -4,11 +4,16 @@ import dotenv from "dotenv";
 import bookroute from "./routes/bookroute.js";
 import userroute from "./routes/userroute.js";
 import cors from "cors";
+
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 const app = express();
+
+//middleware function
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (request, response) => {
   response.send("Hi");
 });
